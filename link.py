7d6c786a261182837d587fe8a56011ed8d1d4701
panditrahulsharma <rahulpandit151197@gmail.com>
2019-07-28 10:25:10 +0530

    find missword,meaningfull sentense

--- link.py
+++ link.py
@@ -1,1 +1,4 @@
-https://github.com/grassknoted/Realtime-Answer-Evaluation/blob/master/PreProcessing.py
+#https://github.com/grassknoted/Realtime-Answer-Evaluation/blob/master/PreProcessing.py
+
+#text blob library
+#https://textblob.readthedocs.io/en/dev/quickstart.html#spelling-correction
